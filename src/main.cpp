cfc81d2d8445bff39a7c2dcc6670748add3e3d22
Andy Mroczkowski <a@mrox.co>
2020-01-11 10:50:42 -0500

    rename path param to dir, and cleanups

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 
+#include <boost/asio.hpp>
 #include <boost/filesystem.hpp>
 #include <docopt.h>
 #include <spdlog/spdlog.h>
@@ -12,14 +13,14 @@ static const char USAGE[] =
     R"(amrox's http server
 
     Usage:
-      amrox_http [--port=<port>] [--path=<path>]
+      amrox_http [--port=<port>] [--dir=<dir>]
       amrox_http (-h | --help)
       amrox_httlp --version
 
     Options:
       -h --help      Show this screen.
       --version      Show version.
-      --path=<path>  Path to server [default: .].
+      --dir=<dir>    Path to root directory to serve [default: .].
       --port=<port>  Port [default: 7777].
 )";
 
@@ -30,12 +31,14 @@ int main(int argc, char **argv)
                              true,                       // show help if requested
                              "amrox's http server 0.1"); // version string
 
-  bf::path path{args["--path"].asString()};
+  bf::path path{args["--dir"].asString()};
   if (!bf::is_directory(path))
   {
     spdlog::error("'{}' is not a directory, aborting.", path.string());
     return 1;
   }
+
+  spdlog::info("Serving root '{}'", bf::canonical(path).string());
 
   boost::asio::io_context io_context;
   short port = static_cast<short>(args["--port"].asLong());
