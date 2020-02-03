ba4730842f18f519b8388e3ae01295add5250a0b
Andy Mroczkowski <amrox@users.noreply.github.com>
2020-02-02 20:24:20 -0500

    inlining ftw

--- include/amrox_http/request.hpp
+++ include/amrox_http/request.hpp
@@ -190,20 +190,8 @@ namespace amrox::http_server {
         friend class Request;
     };
 
-
-//    // -----------------------------------------
-//
-//    auto parse(const std::vector<uint8_t>::const_iterator begin, const std::vector<uint8_t>::const_iterator end) noexcept -> std::optional<Request> {
-//
-//        /*  https://developer.mozilla.org/en-US/docs/Web/HTTP/Messages
-//         *
-//         * HTTP requests, and responses, share similar structure and are composed of:
-//         *      1) A start-line describing the requests to be implemented, or its status of whether successful or a failure. This start-line is always a single line.
-//         *      2) An optional set of HTTP headers specifying the request, or describing the body included in the message.
-//         *      3) A blank line indicating all meta-information for the request have been sent.
-//         *      4) An optional body containing data associated with the request (like content of an HTML form), or the document associated with a response. The presence of the body and its size is specified by the start-line and HTTP headers.
-//         */
-
-
+    inline RequestBuilder Request::make() {
+        return RequestBuilder();
+    }
 }
 
--- src/request.cpp
+++
@@ -1,9 +0,0 @@
-#include "amrox_http/request.hpp"
-
-using namespace amrox::http_server;
-
-RequestBuilder Request::make() {
-    return RequestBuilder();
-}
-
-
