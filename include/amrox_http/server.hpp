477627f59d7d325ae560ca1ca63ec2316609c7ee
Andy Mroczkowski <amrox@users.noreply.github.com>
2020-02-14 15:32:19 -0500

    Cleanups

--- include/amrox_http/server.hpp
+++ include/amrox_http/server.hpp
@@ -61,7 +61,7 @@ class Server
 
 public:
     explicit Server(boost::asio::io_context &io_context, short port)
-        : acceptor(io_context, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), port))
+        : acceptor_(io_context, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), port))
     {
         do_accept();
     }
@@ -69,7 +69,7 @@ public:
 private:
     void do_accept()
     {
-        acceptor.async_accept(
+        acceptor_.async_accept(
             [this](boost::system::error_code ec, boost::asio::ip::tcp::socket socket) {
                 if (!ec)
                 {
@@ -80,6 +80,6 @@ private:
             });
     }
 
-    boost::asio::ip::tcp::acceptor acceptor;
+    boost::asio::ip::tcp::acceptor acceptor_;
 };
 }; // namespace amrox::http_server
